371c1355be7efb4f2207d43fe7feae48a786f252
thomas <Thomas Koehler>
2023-04-23 13:26:19 +0200

    [WIP] Dijkstra-based, need to deal with cycles

--- src/dag_extract.rs
+++ src/dag_extract.rs
@@ -1,13 +1,13 @@
 use crate::*;
 use std::cmp::Ordering;
 use std::fmt::Debug;
-use std::collections::BTreeSet;
+use std::collections::{BTreeSet, BinaryHeap};
 
 /// A cost function to be used by a [`DagExtractor`].
 ///
 /// `Cost` and its operations must define an ordered commutative monoid.
 pub trait DagCostFunction<L: Language> {
-    type Cost: PartialOrd + Debug + Clone + std::ops::AddAssign;
+    type Cost: PartialOrd + Debug + Clone + std::ops::Add<Output=Self::Cost> + std::ops::AddAssign + PartialEq;
 
     /// Returns the zero of Cost.
     fn zero(&mut self) -> Self::Cost;
@@ -25,13 +25,45 @@ impl<L: Language> DagCostFunction<L> for AstSize {
     fn node_cost(&mut self,  _enode: &L) -> Self::Cost { 1 }
 }
 
+type Visited = BTreeSet<Id>;
+type ToVisit = BTreeSet<Id>;
+type SolutionNode = (Visited, ToVisit);
+
+struct State<Cost> {
+    cost: Cost,
+    solution_node: SolutionNode,
+}
+
+// Manually implement Ord so we get a min-heap instead of a max-heap
+// + Allow PartialOrd
+impl<Cost> Ord for State<Cost> where Cost: PartialOrd {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
+impl<Cost> PartialOrd for State<Cost> where Cost: PartialOrd {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        other.cost.partial_cmp(&self.cost)
+    }
+}
+
+impl<Cost> PartialEq for State<Cost> where Cost: PartialEq {
+    fn eq(&self, other: &Self) -> bool {
+        other.cost.eq(&self.cost)
+    }
+}
+
+// TODO: what is this implying?
+impl<Cost> Eq for State<Cost> where Cost: PartialEq {}
+
 #[derive(Debug)]
 pub struct DagExtractor<'a, L: Language, A: Analysis<L>, CF> {
     cost_f: CF,
     // for each eclass,
     //  for each possible set of dependencies,
     //    there exists a best enode, minimizing cost
-    bests: HashMap<Id, HashMap<BTreeSet<Id>, L>>,
+    // bests: HashMap<SolutionNode, CF::Cost>,
     egraph: &'a EGraph<L, A>,
     // TODO: may be able to reduce complexity with immutable sharing of sets
 }
@@ -41,13 +73,13 @@ impl<'a, L, A, CF> DagExtractor<'a, L, A, CF>
           L: 'a, A: 'a, CF::Cost: 'a
 {
   pub fn new(egraph: &'a EGraph<L, A>, cost_f: CF) -> Self {
-    let bests = HashMap::default();
+    // let bests = HashMap::default();
     let mut extractor = DagExtractor {
         cost_f,
-        bests,
+        // bests,
         egraph
     };
-    extractor.find_costs();
+    // extractor.find_costs();
     /* DEBUG:
     for (k, v) in &extractor.bests {
         println!("e-class {:?}: {:?}", k, v);
@@ -56,35 +88,158 @@ impl<'a, L, A, CF> DagExtractor<'a, L, A, CF>
     extractor
   }
 
-  // returns true if parents need to be updated
-  fn update_bests(
-    cost_f: &mut CF,
-    bests: &mut HashMap<Id, HashMap<BTreeSet<Id>, L>>,
-    u_id: Id,
-    u_node: &L,
-    dependencies: BTreeSet<Id>,
-  ) -> bool {
-    use hashbrown::hash_map::Entry;
+  pub fn find_best(&mut self, roots: &[Id]) -> (CF::Cost, RecExpr<L>, Vec<Id>) {
+    // TODO: id for solution nodes
+    let mut bests = HashMap::<SolutionNode, (CF::Cost, Vec<L>, SolutionNode)>::default();
 
-    // TODO: what if there exists dependency subset/superset in map? collapse?
-    let deps_to_best = bests.entry(u_id)
-        .or_insert_with(|| HashMap::default());
-    match deps_to_best.entry(dependencies) {
-        Entry::Vacant(vac) => {
-            vac.insert(u_node.clone());
-            true // parents need to account for new dependencies
+    let mut heap = BinaryHeap::new();
+    // TODO: avoid duplication of sets
+    heap.push(State {
+        cost: self.cost_f.zero(),
+        solution_node: (
+            Visited::default(),
+            roots.iter().map(|&id| self.egraph.find(id)).collect()
+        ),
+    });
+
+    while let Some(State { cost, solution_node }) = heap.pop() {
+        println!("fb: {:?}, {:?}", cost, solution_node);
+        // let (visited, to_visit) = solution_node;
+        // We found a solution, we can stop here.
+        if solution_node.1.is_empty() {
+            return Self::build_solution(cost, solution_node.0, &bests, &self.egraph, roots);
         }
-        Entry::Occupied(mut occ) => {
-            let occ_cost = cost_f.node_cost(occ.get());
-            let new_cost = cost_f.node_cost(u_node);
-            if new_cost < occ_cost {
-                occ.insert(u_node.clone());
+
+        // This path is suboptimal.
+        if (!solution_node.0.is_empty()) && cost > bests[&solution_node].0 {
+            println!("suboptimal");
+            continue;
+        }
+        // TODO: what if = best?
+
+        let n_visited = &solution_node.0 | &solution_node.1;
+        for (n_cost, n_to_visit, n_nodes) in Self::next_moves(cost, solution_node.1.clone(), &self.egraph, &mut self.cost_f) {
+            println!("next move: {:?}, {:?}, {:?}",
+                n_cost, n_to_visit, n_nodes);
+            // This is a cycle.
+            if !n_visited.is_disjoint(&n_to_visit) {
+                println!("cycle");
+                continue;
             }
-            false // parents already accounted for these dependencies
+
+            let next = State {
+                cost: n_cost,
+                solution_node: (n_visited.clone(), n_to_visit),
+                // &n_to_visit - &n_visited = n_to_visit
+            };
+            if bests.get(&next.solution_node).map(|sn| next.cost < sn.0).unwrap_or(true) {
+                println!("push");
+                bests.insert(next.solution_node.clone(), (next.cost.clone(), n_nodes, solution_node.clone()));
+                heap.push(next);
+            }
+        }
+    }
+
+    panic!("extraction failed");
+  }
+
+  fn next_moves(
+    cost: CF::Cost,
+    to_visit: ToVisit,
+    egraph: &EGraph<L, A>,
+    cost_f: &mut CF,
+  ) -> Vec<(CF::Cost, ToVisit, Vec<L>)>
+  {
+    let mut result = Vec::new();
+    let remaining: Vec<Id> = to_visit.into_iter().collect();
+    let mut nodes_so_far = Vec::new();
+    Self::next_moves_rec(&remaining[..], egraph, cost_f, cost, &mut nodes_so_far, ToVisit::default(), &mut result);
+    result
+  }
+
+  fn next_moves_rec(
+    remaining: &[Id],
+    egraph: &EGraph<L, A>,
+    cost_f: &mut CF,
+    cost_so_far: CF::Cost,
+    nodes_so_far: &mut Vec<L>,
+    n_to_visit_so_far: ToVisit,
+    acc: &mut Vec<(CF::Cost, ToVisit, Vec<L>)>,
+  )
+  {
+    match remaining.split_first() {
+        None => acc.push((cost_so_far, n_to_visit_so_far, nodes_so_far.clone())),
+        Some((&tv, rem)) => {
+            let eclass_to_visit = &egraph[tv];
+            for possible_enode in &eclass_to_visit.nodes {
+                debug_assert!(possible_enode.all(|c| egraph.find(c) == c));
+        
+                let cost_so_far = cost_so_far.clone() + cost_f.node_cost(possible_enode);
+                nodes_so_far.push(possible_enode.clone());
+                let children: BTreeSet<_> = possible_enode.children().iter().cloned().collect();
+                let n_to_visit_so_far = &n_to_visit_so_far | &children;
+                Self::next_moves_rec(rem, egraph, cost_f, cost_so_far, nodes_so_far, n_to_visit_so_far, acc);
+                nodes_so_far.pop();
+            }
         }
     }
   }
 
+  fn build_solution(
+    cost: CF::Cost,
+    visited: Visited,
+    bests: &HashMap<SolutionNode, (CF::Cost, Vec<L>, SolutionNode)>,
+    egraph: &EGraph<L, A>,
+    roots: &[Id],
+  ) -> (CF::Cost, RecExpr<L>, Vec<Id>) {
+    println!("visited: {:?}", visited);
+    println!("bs: {:?}", bests);
+    // {0, 1, 3}, {}: 3.1 [B 1, A 0], {3}, {0, 1}
+    // {3}, {0, 1}: 0.1 [list 1 0], {}, {3}
+    let solution_node = &(visited, ToVisit::default());
+    debug_assert!(cost == bests[solution_node].0);
+    let mut expr = RecExpr::default();
+    let mut to_expr = HashMap::default();
+    Self::build_solution_rec(
+        solution_node,
+        bests,
+        egraph,
+        &mut expr,
+        &mut to_expr
+    );
+    (cost, expr, roots.iter().map(|r| to_expr[r]).collect())
+  }
+
+  fn build_solution_rec(
+    solution_node: &SolutionNode,
+    bests: &HashMap<SolutionNode, (CF::Cost, Vec<L>, SolutionNode)>,
+    egraph: &EGraph<L, A>,
+    expr: &mut RecExpr<L>,
+    to_expr: &mut HashMap<Id, Id>,
+  )
+  {
+    println!("bsr");
+    match bests.get(solution_node) {
+        None => (),
+        Some((_, nodes, prev_solution_node)) => {
+            for n in nodes {
+                println!("to_expr: {:?}", to_expr);
+                println!("n: {:?}", n);
+                let new_id = expr.add(n.clone().map_children(|id| to_expr[&id]));
+                to_expr.insert(egraph.lookup(n.clone()).unwrap(), new_id);
+            }
+            Self::build_solution_rec(
+                prev_solution_node,
+                bests,
+                egraph, 
+                expr,
+                to_expr
+            );
+        }
+    }
+  }
+
+/*
   fn find_costs(&mut self) {
     assert!(self.egraph.clean);
 
@@ -106,9 +261,13 @@ impl<'a, L, A, CF> DagExtractor<'a, L, A, CF>
             }
         }
     }
+
+    println!("LEAVES OK");
     
     // work towards parents
     while let Some((enode, eclass_id)) = analysis_pending.pop() {
+        println!("PROCESSING: {:?}, {:?}", enode, eclass_id);
+
         debug_assert!(!enode.is_leaf());
         debug_assert!(self.egraph.find(eclass_id) == eclass_id);
         debug_assert!(enode.all(|c| self.egraph.find(c) == c));
@@ -156,6 +315,35 @@ impl<'a, L, A, CF> DagExtractor<'a, L, A, CF>
                     &dependencies_so_far | child_dependencies,
                 )
             }
+        }
+    }
+  }
+
+  // returns true if parents need to be updated
+  fn update_bests(
+    cost_f: &mut CF,
+    bests: &mut HashMap<Id, HashMap<BTreeSet<Id>, L>>,
+    u_id: Id,
+    u_node: &L,
+    dependencies: BTreeSet<Id>,
+  ) -> bool {
+    use hashbrown::hash_map::Entry;
+
+    // TODO: what if there exists dependency subset/superset in map? collapse?
+    let deps_to_best = bests.entry(u_id)
+        .or_insert_with(|| HashMap::default());
+    match deps_to_best.entry(dependencies) {
+        Entry::Vacant(vac) => {
+            vac.insert(u_node.clone());
+            true // parents need to account for new dependencies
+        }
+        Entry::Occupied(mut occ) => {
+            let occ_cost = cost_f.node_cost(occ.get());
+            let new_cost = cost_f.node_cost(u_node);
+            if new_cost < occ_cost {
+                occ.insert(u_node.clone());
+            }
+            false // parents already accounted for these dependencies
         }
     }
   }
@@ -242,6 +430,7 @@ impl<'a, L, A, CF> DagExtractor<'a, L, A, CF>
     }
     (cost, expr, to_expr)
   }
+  */
 }
 
 
@@ -289,6 +478,8 @@ mod tests {
 
     #[test]
     fn simple_dag_extract() {
+        crate::init_logger();
+
         let mut egraph = EGraph::<S, ()>::default();
         let a = egraph.add(S::leaf("a"));
         let plus = egraph.add(S::new("+", vec![a, a]));
